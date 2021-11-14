8a6abcb8290ca7ae5e067f829b29809de1a8b9dc
calmighted <manishh@tutanota.com>
2021-11-14 11:19:07 +0530

    added delay

--- script.js
+++ script.js
@@ -1,46 +1,40 @@
 const cursor = document.querySelector(".cursor");
-const box = document.querySelector(".box")
-var timeout;
+const box = document.querySelector(".box");
+const svg = document.querySelector("svg");
 
 //follow cursor on mousemove
-box.addEventListener("mousemove", (e) => {
-	let x = e.pageX;
-	let y = e.pageY;
+// box.addEventListener("mousemove", (e) => {
+// 	let x = e.pageX *;
+// 	let y = e.pageY;
+
+// 	cursor.style.top = y + "px";
+// 	cursor.style.left = x + "px";
+// 	cursor.style.display = "block";
+//     svg.style.display = "block";
+
+// });
 
 
-	cursor.style.top = y + "px";
-	cursor.style.left = x + "px";
+let mouseX = 0,
+	mouseY = 0;
+let xp = 0,
+	yp = 0;
+
+box.addEventListener("mousemove", function (e) {
+	mouseX = e.pageX ;
+	mouseY = e.pageY ;
+});
+
+setInterval(function () {
+	xp += (mouseX - xp) / 5;
+	yp += (mouseY - yp) / 5;
+	cursor.style.top = yp + "px";
+	cursor.style.left = xp + "px";
 	cursor.style.display = "block";
+	svg.style.display = "block";
+}, 20);
 
+
+box.addEventListener("mouseout", () => {
+	svg.style.display = "none";
 });
-
-
-document.addEventListener("mouseout", () => {
-	cursor.style.display = "none";
-});
-
-
-
-// 	box.addEventListener("mousemove", getMouse); 
-
-// var eyepos = { x: 0, y: 0 };
-
-// setInterval(followMouse, 50);
-
-// var mouse = { x: 0, y: 0 }; //mouse.x, mouse.y
-
-// function getMouse(e) {
-// 	mouse.x = e.pageX;
-// 	mouse.y = e.pageY;
-// }
-
-// function followMouse() {
-// 	var distX = mouse.x - eyepos.x;
-// 	var distY = mouse.y - eyepos.y;
-// 	eyepos.x += distX / 5;
-// 	eyepos.y += distY / 2;
-
-// 	cursor.style.left = eyepos.x + "px";
-// 	cursor.style.top = eyepos.y + "px";
-//     cursor.style.display = "block";
-// }
